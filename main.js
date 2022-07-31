50217e79ca6c15c97ca0f9c2b27e29d0cca2d890
Laura Kambanis <laurakambanis@lauras-air-3.home>
2022-07-31 15:29:22 +0100

    event listener

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 //time display created with moment.js
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"));
+
+var savedEventLocalStorage;
+var 
+
+saveBtn.addEventListener("click", function) {
+    document.getElementById('myEventInput').value
+    localStorage.setItem
+
+}
+
+var btn = document.getElementById('submit');
+btn.addEventListener('click', func);
+
+function func() {
+  console.log(document.getElementById("number").value)
+}
